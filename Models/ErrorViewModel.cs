620c4c4ea80f3643af8cafb0d48f38c2e9a2ac54
Yatin Khadilkar <ykhadilkar@Yatins-MacBook-Pro.local>
2018-08-21 16:46:39 -0400

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace helloAspDotNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
